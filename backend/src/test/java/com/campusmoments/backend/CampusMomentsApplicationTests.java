a3980dbc469896d3487b7104ef85bde41dc1ddc7
Yw166 <1665845521@qq.com>
2025-11-18 15:35:19 +0800

    feat: 初始化Spring Boot后端项目

---
+++ backend/src/test/java/com/campusmoments/backend/CampusMomentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campusmoments.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusMomentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
